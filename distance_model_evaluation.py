ed37c6d4399c976e3b1f62affb73f3a2cc743eba
Paolo <10699692@polimi.it>
2022-09-20 18:09:20 +0200

    Improved test train splitting function.

--- distance_model_evaluation.py
+++ distance_model_evaluation.py
@@ -3,34 +3,21 @@ from matplotlib import pyplot as plt
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import confusion_matrix, roc_curve, auc, RocCurveDisplay
 
-from heartBeatsExtraction import get_heart_beats
-from mongoDbHb import get_norm_collection, get_anorm_collection
+from heartBeatsExtraction import get_train_test_heart_beats
 
 
 def model_evaluation(given_model, get_distances):
     # number of heart beats to be used for logistic regression
     log_reg_hb_size = 5000
-    # normal heart beats collection from database
-    norm_col = get_norm_collection()
-    # split heart beats in different classes
-    norm_train_heart_beats, norm_train_classes, norm_test_heart_beats, norm_test_classes = get_heart_beats(
-        norm_col, log_reg_hb_size)
-    # anormal heart beats collection from database
-    anorm_col = get_anorm_collection()
-    # split heart beats in different classes
-    anorm_train_heart_beats, anorm_train_classes, anorm_test_heart_beats, anorm_test_classes = get_heart_beats(
-        anorm_col, log_reg_hb_size)
-    # subdivision of heart beats
-    train_heart_beats = np.vstack((norm_train_heart_beats, anorm_train_heart_beats))
-    train_classes = np.append(norm_train_classes, anorm_train_classes)
+    # get train and test heart beats and classes
+    train_heart_beats, train_classes, test_heart_beats, test_classes = get_train_test_heart_beats(log_reg_hb_size)
+    # get the model specific distances
     train_distances = get_distances(given_model, train_heart_beats)
     # training of linear regression model
     print('Starting fitting')
     model = LogisticRegression(solver='liblinear', C=10.0, random_state=0).fit(train_distances, train_classes)
     print('Finished fitting')
-    # subdivision of heart beats
-    test_heart_beats = np.vstack((norm_test_heart_beats, anorm_test_heart_beats))
-    test_classes = np.append(norm_test_classes, anorm_test_classes)
+    # get the model specific distances
     test_distances = get_distances(given_model, test_heart_beats)
     # prediction of the model with confusion matrix
     ans = model.predict(test_distances)
--- heartBeatsExtraction.py
+++ heartBeatsExtraction.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+from mongoDbHb import get_norm_collection, get_anorm_collection
+
 
 def get_arrays(coll):
     heart_beats = np.zeros(150)
@@ -22,3 +24,22 @@ def get_heart_beats(col, num_of_hb: int):
     test_heart_beats_local, test_classes_local = get_arrays(test_coll)
 
     return train_heart_beats_local, train_classes_local, test_heart_beats_local, test_classes_local
+
+
+def get_train_test_heart_beats(size: int):
+    # normal heart beats collection from database
+    norm_col = get_norm_collection()
+    # split heart beats in different classes
+    norm_train_heart_beats, norm_train_classes, norm_test_heart_beats, norm_test_classes = get_heart_beats(
+        norm_col, size)
+    # anormal heart beats collection from database
+    anorm_col = get_anorm_collection()
+    # split heart beats in different classes
+    anorm_train_heart_beats, anorm_train_classes, anorm_test_heart_beats, anorm_test_classes = get_heart_beats(
+        anorm_col, size)
+    # subdivision of heart beats
+    train_heart_beats = np.vstack((norm_train_heart_beats, anorm_train_heart_beats))
+    train_classes = np.append(norm_train_classes, anorm_train_classes)
+    test_heart_beats = np.vstack((norm_test_heart_beats, anorm_test_heart_beats))
+    test_classes = np.append(norm_test_classes, anorm_test_classes)
+    return train_heart_beats, train_classes, test_heart_beats, test_classes
